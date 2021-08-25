41449193aa62a14678829fe3b5ecb757e8b2ff7f
BrentSnoddy <brentsnoddy1@gmail.com>
2021-08-25 18:58:31 -0400

    MVC Address Book App with Heroku database

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MVCAddressBook.Data;
+using System.Threading.Tasks;
+
+namespace MVCAddressBook
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var dbContext = host.Services
+                                .CreateScope().ServiceProvider
+                                .GetRequiredService<ApplicationDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
